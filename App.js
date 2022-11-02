9ad11a02ce50d85660adb3bf3f3153fa3d0a5802
Sébastien <toxicsed@gmail.com>
2022-11-02 20:08:10 +0000

    update

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Lottie from 'lottie-react-native';
+import { Animated, Easing } from 'react-native';
 
+ 
 export default function App() {
+    
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>Help Others</Text>
       <StatusBar style="auto" />
-    </View>
+      
+        <Lottie
+          style={styles.animation}
+          source={require('./assets/animation.json')}
+          autoPlay
+          loop
+          
+        /> 
+      </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:'#e61f09',
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
+
+  title: {
+    fontSize: 70,
+    fontWeight: 'bold',
+  },
+
+  lottie: {
+    width: 100,
+    height: 100,
+  },
+
+  animationContainer: {
+    backgroundColor: 'transparent',
+    width: 200,
+    height: 200,
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+
 });
