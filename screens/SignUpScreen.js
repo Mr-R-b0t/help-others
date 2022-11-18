f39fb3cbbf44f882871aa6bfb96ce970adb18679
Sébastien <52678042+Mr-R-b0t@users.noreply.github.com>
2022-11-18 16:35:15 +0100

    update
    adding firebase to project by building the app for android and ios

---
+++ screens/SignUpScreen.js
@@ -0,0 +1,99 @@
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Image } from 'react-native'
+import React, { useState, useLayoutEffect } from 'react'
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { useNavigation } from '@react-navigation/native';
+import { ChevronDownIcon, HomeIcon } from "react-native-heroicons/outline"
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const SignIn = () => {
+
+    const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    })
+  }, [])
+
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const handleSubmit = async () => {
+        if (name === '' || email === '' || password === '') {
+            alert("All fields are required");
+            return;
+        }
+        await axios.post("http://localhost:8001/api/signin", { name, email, password });
+        alert("Sign In Successful");
+    };
+    return (
+        <SafeAreaView>
+            <View className="flex-row pb-3 items-center mx-4 space-x-2 px-1">
+                <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+                    <Image source={{ uri: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png" }} className=" h-7 w-7 bg-gray-300 p-4 rounded-full" />
+                </TouchableOpacity>
+                <View className="flex-1 mx-1">
+                    <Text className="font-bold text-3xl">Help Others<ChevronDownIcon size={20} className="ml-2" /></Text>
+                </View>
+                <HomeIcon className="ml-2" size={35} onPress={() => navigation.navigate('Home')} />
+            </View>
+        <KeyboardAwareScrollView contentCotainerStyle={styles.container} className="pt-14">
+            <View style={{ marginVertical: 100 }}>
+            <View style={styles.imageContainer}>
+                <Image source={{uri: "https://cdn.pixabay.com/photo/2020/07/01/12/58/icon-5359553_1280.png"} } style={styles.imageStyles} />
+            </View>
+                <Text style={styles.signupText}>Login</Text>
+                <View style={{ marginHorizontal: 24 }}>
+                    <Text style={{ fontSize: 16, color: '#8e93a1' }}>EMAIL</Text>
+                    <TextInput style={styles.signupInput} value={email} onChangeText={text => setEmail(text)} autoCompleteType="email" keyboardType="email-address" />
+                </View>
+                <View style={{ marginHorizontal: 24 }}>
+                    <Text style={{ fontSize: 16, color: '#8e93a1' }}>PASSWORD</Text>
+                    <TextInput style={styles.signupInput} value={password} onChangeText={text => setPassword(text)} secureTextEntry={true} autoComplteType="password" />
+                </View>
+                <TouchableOpacity onPress={handleSubmit} style={styles.buttonStyle}>
+                    <Text style={styles.buttonText}>Submit</Text>
+                </TouchableOpacity>
+                    
+            </View>
+        </KeyboardAwareScrollView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    signupText: {
+        fontSize: 30,
+        textAlign: 'center'
+    },
+    signupInput: {
+        borderBottomWidth: 0.5,
+        height: 48,
+        borderBottomColor: "#8e93a1",
+        marginBottom: 30,
+    },
+    buttonStyle: {
+        backgroundColor: "darkmagenta",
+        height: 50,
+        marginBottom: 20,
+        justifyContent: "center",
+        marginHorizontal: 15,
+        borderRadius: 15,
+    },
+    buttonText: {
+        fontSize: 20,
+        textAlign: 'center',
+        color: '#fff',
+        textTransform: 'uppercase',
+        fontWeight: 'bold'
+    },
+    imageContainer: { justifyContent: "center", alignItems: "center" },
+    imageStyles: { width: 100, height: 100, marginVertical: 20 }
+})
+
+export default SignIn
