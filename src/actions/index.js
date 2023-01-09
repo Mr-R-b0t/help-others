1c9061cccc5a072a3586116a2984a1f465779f3e
Sébastien <52678042+Mr-R-b0t@users.noreply.github.com>
2023-01-10 00:17:55 +0100

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,6 @@ import {Client} from 'appwrite';
 
 const appwrite = new Client();
 appwrite
-  .setEndpoint("https://appwrite.le-app.dev/v1") // Your API Endpoint
-  .setProject("63b6a0ca89fafa3f0baa"); // Your project IDs
+  .setEndpoint("") // Your API Endpoint
+  .setProject(""); // Your project IDs
 export const appwriteClient = appwrite; 
