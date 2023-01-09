9b4ea2a21ec2b11e0dc6c15b1cfaaa130d95a67d
Sébastien <52678042+Mr-R-b0t@users.noreply.github.com>
2023-01-09 14:11:36 +0100

    update

--- screens/sos.js
+++ screens/sos.js
@@ -68,7 +68,7 @@ const sos = () => {
             onPress={() => navigation.navigate("sos2")}
           >
             <Text className="font-bold mx-2 py-2  items-center text-3xl">
-              Je sais pas
+              Je ne sais pas
             </Text>
           </TouchableOpacity>
         </View>
